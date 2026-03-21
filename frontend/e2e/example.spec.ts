f6dbe917b73b094b86462702da3e6acc4b26dcab
Manfrin <manfredd_10@hotmail.com>
2026-03-21 12:34:48 -0300

    feat: install playwright

---
+++ frontend/e2e/example.spec.ts
@@ -0,0 +1,8 @@
+import { test, expect } from '@playwright/test';
+
+test('get started link', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started' }).click();
+});
